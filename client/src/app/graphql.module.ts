57b598e031f33ce8cea67c81351c83dbc3df86ee
Yehuda Ringler <yrappdev@gmail.com>
2021-01-25 14:06:11 -0500

    Use angular persisted queries

--- client/src/app/graphql.module.ts
+++ client/src/app/graphql.module.ts
@@ -1,22 +1,25 @@
 import { NgModule, PLATFORM_ID } from '@angular/core';
 import { APOLLO_OPTIONS } from 'apollo-angular';
-import { ApolloClientOptions, ApolloLink, createHttpLink, InMemoryCache } from '@apollo/client/core';
+import { ApolloClientOptions, ApolloLink, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
-import { createPersistedQueryLink } from 'apollo-link-persisted-queries';
+import { createPersistedQueryLink } from 'apollo-angular-link-persisted';
 import { environment } from './../environments/environment';
 import { isPlatformServer } from '@angular/common';
 
 const uri = `${environment.apiURL}/graphql`;
 
-export function createApollo(angularLink: HttpLink, platformId:any): ApolloClientOptions<any> {
+export function createApollo(httpLink: HttpLink, platformId: any): ApolloClientOptions<any> {
   const isSSr = isPlatformServer(platformId);
+  //const link = createPersistedQueryLink({ useGETForHashedQueries: true }).concat(httpLink.create({uri}));
+
+  const link = ApolloLink.from([
+    createPersistedQueryLink({ useGETForHashedQueries: true }) as any,
+    httpLink.create({ uri })
+  ])
 
   return {
     ssrMode: isSSr,
-    link: isSSr ? angularLink.create({uri}) : ApolloLink.from([
-      createPersistedQueryLink({ useGETForHashedQueries: true }) as any,
-      createHttpLink({ uri })
-    ]),
+    link,
     cache: new InMemoryCache(),
     defaultOptions: {
       watchQuery: {
